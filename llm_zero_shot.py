b53f776a51630046142adb9f3908332d719c52cd
TomasUOM <mistrmalik@gmail.com>
2024-11-24 16:24:58 -0600

    code update

--- llm_zero_shot.py
+++ llm_zero_shot.py
@@ -3,6 +3,7 @@ import datasets
 from  huggingface_hub import login
 # import nltk
 import vllm.sampling_params
+import re
 
 # keeping my read token private
 with open("token.txt", 'r') as f:
@@ -38,27 +39,51 @@ def load_data(dataset_name):
     data = datasets.load_dataset(dataset_name, trust_remote_code = True)
     return data
 
-def prompt_model(question, options): #tbd later, using format 
+def prompt_model(source, question, options): #tbd later, using format 
     # you are an expert in _____,  (essentially description of the AI's role: e.g.: "You are a helpful assistant")
     system_prompt = "You are a helpful assistant." 
     # what the model is actually being tasked with: different for each question in benchmark
-    user_prompt = f""
+    user_prompt = f"Please choose an appropriate answer from the four options, given the following Source material, Question, and Options."
+    user_prompt = user_prompt + f"The Source material is: {source}/n/n" + f"The Question is: {question}\n\n" + f"The options are as follows:\n\n Option 1: {options[0]}\n\n Option 2 {options[1]}\n\n Option 3: {options[2]}\n\n Option 4: {options[3]}\n\n"
     # here I usually specify format for the model
-    instructions = ""
+    instructions = "Clarify your decision by returning the number, which represents your chosen option, at the end of your message."
     return system_prompt, user_prompt, instructions
 
-def generate(source_text, question, system_prompt, user_prompt, instructions, llm, nsampling = 1):
+def generate(system_prompt, user_prompt, instructions, llm, nsampling = 1):
     t = 0.0 if nsampling == 1 else 0.8 # 0.8 should be probably sampled on validatoin set first, but it's a start
     #source where it will look for an answer, the prompts, nsampling is for self consistency (statistical decoding technique)
     request = f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n{user_prompt}\
-        \n**Source:** \"{source_text}\"\n\n **Instructions:** \n{instructions} <|eot_id|><|start_header_id|>assistant<|end_header_id|>\n["
+        \n\n**Instructions:** \n{instructions} <|eot_id|><|start_header_id|>assistant<|end_header_id|>\n["
     params = vllm.sampling_params(n = nsampling, temperature = t, seed = 42, max_tokens = 500) # 500 is max num of tokens in its answer
     # seed is random seed
     # may later include logprobs if of interest
     output = llm.generate(request, params, use_tqdm = True) # trying use_tqdm true for the first time, dunno how it will work
     return output[0].outputs[0].text #returns only the raw answer text
 
+def last_int(text):
+    integers = re.findall(r'\d+', text)
+    if integers:
+        return int(integers[-1])
+    else:
+        return -1
+
 # def predict(source, question, options):
+def predict(source, question, options, llm):
+    ans_full = []
+    num_ans = []
+    ext_failed = 0
+    for a,b,c in zip(source, question, options):
+        sp, up, i = prompt_model(a,b,c)
+        text = generate(sp, up, i, llm)
+        ans_full.append(text)
+        num = last_int(text)
+        num_ans.append(num)
+        if num == -1:
+            ext_failed += 1
+    print(f"Extraction failed {ext_failed} times!")
+    return ans_full, num_ans
+
+
 
 
 
@@ -72,9 +97,20 @@ def main():
     data = load_data(dataset)
 
     data_train = data['train']
-    data_val = data['validation'][:999]
+    data_val = data['validation'][0]
     data_test = data['validation'][1000:]
 
+    test = data_val
+    source = test['article']
+    question = test['question']
+    options = test['options']
+    target = test['answer']
+
+    print(data_val)
+    print(source)
+    print(question)
+    print(options)
+    print(target)
 
 
     # print(data_train[0])
