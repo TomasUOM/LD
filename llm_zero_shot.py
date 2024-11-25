54f7c602d165b3eb6652aecdecbce0a49fdf940f
TomasUOM <mistrmalik@gmail.com>
2024-11-24 23:03:11 -0600

    update

--- llm_zero_shot.py
+++ llm_zero_shot.py
@@ -1,8 +1,7 @@
-import vllm
+from vllm import SamplingParams, LLM
 import datasets
 from  huggingface_hub import login
 # import nltk
-import vllm.sampling_params
 import re
 
 # keeping my read token private
@@ -29,7 +28,7 @@ def choose_model(name):
     # tensor_parallel_size = 1 means that only one gpu will be used - changing this based on gpu availability
     # gpu memory util: you want a high (close to 1) to make sure the model fits (not sure) but you may overload the GPU in doing so
     # this param depends on the GPU (and their number) used for training
-    llm = vllm.LLM(model = name, gpu_memory_utilization = 0.9, max_model_len = 40000, tensor_parallel_size = 1) 
+    llm = LLM(model = name, gpu_memory_utilization = 0.95, max_model_len = 40000, tensor_parallel_size = 1) 
     return llm
 
 def load_data(dataset_name):
@@ -46,7 +45,7 @@ def prompt_model(source, question, options): #tbd later, using format
     user_prompt = f"Please choose an appropriate answer from the four options, given the following Source material, Question, and Options."
     user_prompt = user_prompt + f"The Source material is: {source}/n/n" + f"The Question is: {question}\n\n" + f"The options are as follows:\n\n Option 1: {options[0]}\n\n Option 2 {options[1]}\n\n Option 3: {options[2]}\n\n Option 4: {options[3]}\n\n"
     # here I usually specify format for the model
-    instructions = "Clarify your decision by returning the number, which represents your chosen option, at the end of your message."
+    instructions = "Clarify your decision by returning the number, which represents your chosen option, at the end of your message in curly brackets, for example {1}."
     return system_prompt, user_prompt, instructions
 
 def generate(system_prompt, user_prompt, instructions, llm, nsampling = 1):
@@ -54,10 +53,10 @@ def generate(system_prompt, user_prompt, instructions, llm, nsampling = 1):
     #source where it will look for an answer, the prompts, nsampling is for self consistency (statistical decoding technique)
     request = f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n{user_prompt}\
         \n\n**Instructions:** \n{instructions} <|eot_id|><|start_header_id|>assistant<|end_header_id|>\n["
-    params = vllm.sampling_params(n = nsampling, temperature = t, seed = 42, max_tokens = 500) # 500 is max num of tokens in its answer
+    params = SamplingParams(n = nsampling, top_k=-1,min_p=0.4,temperature = t, seed = 42, max_tokens = 500) # 500 is max num of tokens in its answer
     # seed is random seed
     # may later include logprobs if of interest
-    output = llm.generate(request, params, use_tqdm = True) # trying use_tqdm true for the first time, dunno how it will work
+    output = llm.generate(request,params, use_tqdm = False) # trying use_tqdm true for the first time, dunno how it will work
     return output[0].outputs[0].text #returns only the raw answer text
 
 def last_int(text):
@@ -72,7 +71,10 @@ def predict(source, question, options, llm):
     ans_full = []
     num_ans = []
     ext_failed = 0
+    y = 1
     for a,b,c in zip(source, question, options):
+        print(f"Question {y} in progress...")
+        y += 1
         sp, up, i = prompt_model(a,b,c)
         text = generate(sp, up, i, llm)
         ans_full.append(text)
@@ -84,7 +86,19 @@ def predict(source, question, options, llm):
     return ans_full, num_ans
 
 
+def precision(num_ans, target):
+    total = len(target)
+    acc = 0
+    for i in range(len(num_ans)):
+        if num_ans[i]-1 == target[i]:
+            acc += 1
+    print(f"Accuracy: {acc} out of {total}")
+    if total != 0:
 
+        print(f"Accuracy: {acc/total}")
+        return acc/total
+    else:
+        return 0
 
 
 
@@ -93,11 +107,12 @@ def predict(source, question, options, llm):
 def main():
     model = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     dataset = "emozilla/quality"
+    
 
     data = load_data(dataset)
 
     data_train = data['train']
-    data_val = data['validation'][0]
+    data_val = data['validation'][:1000]
     data_test = data['validation'][1000:]
 
     test = data_val
@@ -106,11 +121,14 @@ def main():
     options = test['options']
     target = test['answer']
 
-    print(data_val)
-    print(source)
-    print(question)
-    print(options)
+    llm=choose_model(model)
+    
+    ans_full, num_ans = predict(source, question, options, llm)
+    print(ans_full[:10])
+    print(num_ans)
     print(target)
+    precision(num_ans, target)
+    
 
 
     # print(data_train[0])
